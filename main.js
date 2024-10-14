039dc5f064f972f51b72b543560b5f190b2c4a7a
p-ortilho <cainportilho@gmail.com>
2024-10-14 19:24:54 -0300

    melhorias da otimização do loop for

--- main.js
+++ main.js
@@ -5,35 +5,30 @@ function tocaSom (seletorAudio) {
         elemento.play();
     }
     else {
-        //alert('Elemento não encontrado');
-        console.log('Elemento não encontrado ou seletor inválido');
+        console.error('Elemento não encontrado ou seletor inválido');
     }
 
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-//para
-for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+for(const tecla of listaDeTeclas) {
 
-    const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-    const idAudio = `#som_${instrumento}`; //template string
+    const idAudio = `#som_${instrumento}`; // template string
 
-    tecla.onclick = function () {
+    tecla.onclick = function(){
         tocaSom(idAudio);
-    }
+    };
 
     tecla.onkeydown = function (evento) {
-
-        if (evento.code === 'Space' || evento.code === 'Enter') {
+        if(evento.code === 'Enter' || evento.code === 'Space'){
             tecla.classList.add('ativa');
         }
-
     }
 
     tecla.onkeyup = function () {
         tecla.classList.remove('ativa');
     }
-
+    
 }
